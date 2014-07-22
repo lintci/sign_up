6fa25bd3b3a57d28da9fbe36d8268dd7c54516a7
Allen Madsen <blatyo@gmail.com>
2014-07-22 08:06:25 -0400

    Redirect www to non-www.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  constraints subdomain: 'www' do
+    get ':any', to: redirect(subdomain: nil, path: '/%{any}'), any: /.*/
+  end
+
   root to: 'visitors#show'
 
   resources :visitors, only: [:index, :create]
